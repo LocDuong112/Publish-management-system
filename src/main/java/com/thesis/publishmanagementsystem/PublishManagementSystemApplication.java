677a724ec1bbeb500a818ca11d8ab92d4f311c61
DuongMinhLoc <duongminhloc201@gmail.com>
2022-11-25 10:19:30 +0700

    Initialize project

---
+++ src/main/java/com/thesis/publishmanagementsystem/PublishManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.thesis.publishmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublishManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PublishManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thesis/publishmanagementsystem/PublishManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thesis.publishmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublishManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
